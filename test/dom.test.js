d7af0c78fb6c53361e09ec8ffae766eba0c0435a
The Metaist <metaist@metaist.com>
2018-05-16 20:29:05 -0400

    fix: duil.List returns DOM instead of jQuery (closes #20)

--- test/dom.test.js
+++ test/dom.test.js
@@ -11,17 +11,17 @@ test('dom.find', (t) => {
   const html = '<div><ul><li>First</li><li>Second</li></ul></div>';
   const $dom = $(html);
   const query = 'li';
-  const $result = duil.dom.find($dom, query);
+  const $result = duil._dom.find($dom, query);
 
-  t.is($result[0], duil.dom.find($dom[0], query));
+  t.is($result[0], duil._dom.find($dom[0], query));
   t.is($result.text(), 'First');
 
   t.end();
 });
 
 test('dom.remove null', (t) => {
-  t.is(duil.dom.remove(), null);
-  t.is(duil.dom.remove(null), null);
+  t.is(duil._dom.remove(), null);
+  t.is(duil._dom.remove(null), null);
 
   t.end();
 });
@@ -30,13 +30,13 @@ test('dom.remove', (t) => {
   const html = '<div><ul><li>First</li><li>Second</li></ul></div>';
   const $dom = $(html);
   const query = 'li';
-  const $tmpl = duil.dom.find($dom, query);
+  const $tmpl = duil._dom.find($dom, query);
 
-  t.is($tmpl, duil.dom.remove($tmpl));
+  t.is($tmpl, duil._dom.remove($tmpl));
 
   const dom = $(html)[0];
-  const tmpl = duil.dom.find(dom, query);
-  t.is(tmpl, duil.dom.remove(tmpl));
+  const tmpl = duil._dom.find(dom, query);
+  t.is(tmpl, duil._dom.remove(tmpl));
 
   t.end();
 });
@@ -44,8 +44,16 @@ test('dom.remove', (t) => {
 test('dom.text', (t) => {
   const text = 'text';
   const $dom = $('<div>');
-  duil.dom.setText($dom, text);
+  duil._dom.setText($dom, text);
   t.is($dom.text(), text);
+
+  t.end();
+});
+
+
+test('dom.getIndex', (t) => {
+  const $dom = $('<p>1</p><p>2</p><p>3</p>');
+  t.is('2', duil._dom.getIndex($dom, 1).text());
 
   t.end();
 });
